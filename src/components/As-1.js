a8758ce0a82f03fc8c92686c4dd42a26edc7e53a
ramya1102 <ramyapanditi@gmail.com>
2023-10-14 22:43:53 +0530

    first commit

---
+++ src/components/As-1.js
@@ -0,0 +1,31 @@
+import {Component} from "react";
+import "./style.css"
+class As1 extends Component{
+    constructor(props){
+        super(props);
+        this.state={count:0};
+    }
+    handleChange = (event) => {
+        let word = event.target.value;
+        if (word === "") {  
+          this.setState({
+            count: 0,
+          });
+        } else {
+          var arr = word.trim().split(/\s+/);
+          this.setState({
+            count: arr.length,
+          });
+        }
+      }      
+    render(){
+        return(
+<div class="a1">
+    <h1>Responsive Paragraph Word Counter</h1>
+    <textarea placeholder="Enter text here" onChange={this.handleChange}></textarea>
+    <p>Word Count:{this.state.count}</p>
+</div>
+        );
+    }
+}
+export default As1;
